6b81f314cd5133f51ea75326b0b4e5de7147ef72
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-04-16 10:08:24 -0300

    feat: teams and trophies sections description

--- app/sections/Teams.tsx
+++ app/sections/Teams.tsx
@@ -1,3 +1,5 @@
+import { BiTimeFive } from 'react-icons/bi';
+
 import teams from '@/constants/teams';
 import TeamCard from '@/components/teams/TeamCard';
 import SectionTitle from '@/components/shared/SectionTitle';
@@ -6,8 +8,13 @@ const Teams = () => {
 	return (
 		<section id='#teams' className='mt-20'>
 			<div className="max-w-[1440px] w-full m-auto p-5">
-				<div className="flex flex-col items-center mb-6">
+				<div className="flex flex-col items-center text-center mb-6 gap-6">
 					<SectionTitle title="Equipos" />
+					<p className='text-white text-[17px] md:text-[19px] lg:text-[21px] leading-[27px]'>Un recorrido por los equipos donde Lionel Messi ha jugado, con sus <span className='font-marker text-gradient-gold'>impresionantes estadísticas</span> que demuestran su impacto en el fútbol mundial.</p>
+					<div className='flex items-center text-center gap-4'>
+						<BiTimeFive className='text-blue text-[20px] hidden sm:flex'/>
+						<p className='text-gray-500 text-[18px] italic'>Datos actualizados al 16/04/2023</p>
+					</div>
 				</div>
 				<ul className='grid grid-cols-12 gap-4 md:gap-6 lg:gap-10'>
 					{teams.map((team) => (
--- app/sections/Trophies.tsx
+++ app/sections/Trophies.tsx
@@ -1,13 +1,22 @@
+import { AiOutlineLink } from 'react-icons/ai';
+import Link from 'next/link';
+
 import { argentinaTrophies, barcelonaTrophies, parisTrophies } from '@/constants/trophies';
 import TrophiesContainer from '@/components/trophies/TrophiesContainer';
 import SectionTitle from '@/components/shared/SectionTitle';
+
 
 const Trophies = () => {
 	return (
 		<section id='#trophies' className='mt-20'>
 			<div>
-				<div className='w-full flex justify-center mb-10'>
+				<div className='w-full flex flex-col items-center text-center gap-6 p-5'>
 					<SectionTitle title='Trofeos'/>
+					<p className='text-white text-[17px] md:text-[19px] lg:text-[21px] leading-[27px]'>La impresionante lista de trofeos de Lionel Messi, reflejo de su <span className='font-marker text-gradient-gold'>incomparable habilidad</span> y dedicación en el fútbol.</p>
+					<Link href="https://es.wikipedia.org/wiki/Anexo:Palmar%C3%A9s_y_distinciones_individuales_de_Lionel_Messi#Distinciones_individuales" className='flex items-center text-center gap-4'>
+						<AiOutlineLink className='text-blue text-[20px] hidden sm:flex'/>
+						<p className='text-gray-500 text-[18px] italic'>Consultá las distinciones individuales de Messi acá</p>
+					</Link>
 				</div>
 				<TrophiesContainer gradient='barcelona-gradient' teamName='FC Barcelona' trophies={barcelonaTrophies}/>
 				<TrophiesContainer gradient='psg-gradient' teamName='Paris Saint Germain' trophies={parisTrophies}/>
