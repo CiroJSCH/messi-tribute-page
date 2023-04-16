cd2885f8907d80e43885c4e099eea84a81ade78c
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-04-16 15:05:35 -0300

    feat: added footer

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,25 @@ import Banner from './sections/Banner';
 export default function Home() {
 	return (
 		<>
-			<header className='overflow-x-hidden'>
+			<header className="overflow-x-hidden">
 				<Navigation />
 				<Banner />
 			</header>
-			<main className='main-background'>
+			<main className="main-background pb-[75px]">
 				<Biography />
 				<Teams />
 				<Trophies />
 				<Highlights />
 			</main>
+			<footer className='pb-[15px] bg-black'>
+				<div className='bg-black border-t-2 border-t-gold py-3' />
+				<section className="py-4 px-1 gold-gradient">
+					<p className="text-black font-medium text-center uppercase">
+            © {new Date().getFullYear()} | Desarrollado por Ciro Schapert
+					</p>
+				</section>
+				<div className='bg-black border-b-2 border-b-gold py-3' />
+			</footer>
 		</>
 	);
 }
