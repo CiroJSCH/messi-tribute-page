4dd7cd125ef68d07dac6c71044deb38098bd0010
CiroJSCH <104742538+CiroJSCH@users.noreply.github.com>
2023-04-16 09:35:27 -0300

    feat: highlights description

--- app/sections/Highlights.tsx
+++ app/sections/Highlights.tsx
@@ -1,4 +1,6 @@
 
+import { AiFillInfoCircle } from 'react-icons/ai';
+
 import highlightsTop from '@/constants/highlightsTop';
 import Item from '@/components/highlights/Item';
 
@@ -8,8 +10,13 @@ const Highlights = () => {
 	return (
 		<section>
 			<div className='flex flex-col w-full max-w-[1440px] items-center mx-auto p-5'>
-				<div>
+				<div className='flex flex-col items-center text-center gap-6'>
 					<SectionTitle title="Destacados"/>
+					<p className='text-white text-[17px] md:text-[19px] lg:text-[21px] leading-[27px]'>Messi ha tenido muchisimos <span className='font-marker text-gradient-gold'>momentos inolvidables</span> pero, a mi parecer, estos cinco son los más destacados:</p>
+					<div className='flex items-center text-center gap-4'>
+						<AiFillInfoCircle className='text-blue text-[20px] hidden sm:flex'/>
+						<p className='text-gray-500 text-[18px] italic'>Tocá la imagen para ver la información</p>
+					</div>
 				</div>
 				<section className='w-full mt-10'>
 					<ol className='grid grid-cols-12 grid-rows-5 sm:grid-rows-3 h-[1500px] sm:h-[900px] gap-4 md:gap-5 xl:gap-6'>
